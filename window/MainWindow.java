29ec99d3d412b218ff19e385d068bbe890d7e8e0
miyagawahiroto <100072291+miyagawahiroto@users.noreply.github.com>
2022-06-03 18:46:00 +0900

    画像大きさ変更とボタンで画像変更

--- window/MainWindow.java
+++ window/MainWindow.java
@@ -3,14 +3,18 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MainWindow {
-    //メッセージを表示するラベル
-  private JLabel messageLabel;
-    //画像を表示するラベル
-  private JLabel gazouLabel;
-    //アイコン作成
-  ImageIcon icon1 = new ImageIcon("img/1S");
     //ゲームを表示するフレーム
     private JFrame frame;
+    //メッセージを表示するラベル
+    private JLabel messageLabel;
+    //画像を表示するラベル
+    private JLabel gazouLabel;
+    //アイコン作成
+    ImageIcon icon = new ImageIcon("./img/ura.png");
+    ImageIcon icon1 = new ImageIcon("./img/1S.png");
+    //ボタン作成
+    private final JButton s1Button;
+    
     
   //コンストラクタ
   public MainWindow(){
@@ -35,6 +39,12 @@ public class MainWindow {
         gazouLabel.setBounds(250, 150, 100, 100);
         gazouLabel.setIcon(icon1);
         canvas.add(gazouLabel);
+
+        //ボタン
+        this.s1Button =new JButton(icon);
+        this.s1Button.setBounds(100, 100, 68, 100);
+        this.s1Button.addActionListener((e) -> s1Button.setIcon(icon1));
+        canvas.add(this.s1Button);
 
         // レイアウトマネージャの指定
         this.frame.setLayout(new BorderLayout());
