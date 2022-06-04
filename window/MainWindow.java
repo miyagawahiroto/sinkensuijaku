c7bb0dd31a52c1bee9fa5dbcc297076fdd35e37b
miyagawahiroto <100072291+miyagawahiroto@users.noreply.github.com>
2022-06-04 14:15:46 +0900

    乱数でカードの数字をランダム変更成功

--- window/MainWindow.java
+++ window/MainWindow.java
@@ -1,11 +1,11 @@
 package window;
 import java.awt.*;
 import javax.swing.*;
-import window.num.ransuu;
+import window.num.*;
 
 public class MainWindow {
     //ゲームを表示するフレーム
-    private JFrame frame;
+    public JFrame frame;
     //メッセージを表示するラベル
     private JLabel messageLabel;
     //画像を表示するラベル
@@ -38,7 +38,6 @@ public class MainWindow {
     
     //ボタン用乱数生成
     int[] x=new int[10];
-    String[] ico={"icon1","icon2","icon3","icon4","icon5","icon6","icon7","icon8","icon9","icon10"};
     
     //コンストラクタ
     public MainWindow(){
@@ -70,52 +69,52 @@ public class MainWindow {
         //ボタン
         this.s1Button =new JButton(icon);
         this.s1Button.setBounds(100, 100, 68, 100);
-        this.s1Button.addActionListener((e) -> s1Button.setIcon(icon1));
+        this.s1Button.addActionListener((e) -> s1Button.setIcon(Bottonsakusei(x[0])));
         canvas.add(this.s1Button);
 
         this.s2Button =new JButton(icon);
         this.s2Button.setBounds(170, 100, 68, 100);
-        this.s2Button.addActionListener((e) -> s2Button.setIcon(icon2));
+        this.s2Button.addActionListener((e) -> s2Button.setIcon(Bottonsakusei(x[1])));
         canvas.add(this.s2Button);
 
         this.s3Button =new JButton(icon);
         this.s3Button.setBounds(240, 100, 68, 100);
-        this.s3Button.addActionListener((e) -> s3Button.setIcon(icon3));
+        this.s3Button.addActionListener((e) -> s3Button.setIcon(Bottonsakusei(x[2])));
         canvas.add(this.s3Button);
 
         this.s4Button =new JButton(icon);
         this.s4Button.setBounds(310, 100, 68, 100);
-        this.s4Button.addActionListener((e) -> s4Button.setIcon(icon4));
+        this.s4Button.addActionListener((e) -> s4Button.setIcon(Bottonsakusei(x[3])));
         canvas.add(this.s4Button);
 
         this.s5Button =new JButton(icon);
         this.s5Button.setBounds(380, 100, 68, 100);
-        this.s5Button.addActionListener((e) -> s5Button.setIcon(icon5));
+        this.s5Button.addActionListener((e) -> s5Button.setIcon(Bottonsakusei(x[4])));
         canvas.add(this.s5Button);
 
         this.s6Button =new JButton(icon);
         this.s6Button.setBounds(100, 250, 68, 100);
-        this.s6Button.addActionListener((e) -> s6Button.setIcon(icon6));
+        this.s6Button.addActionListener((e) -> s6Button.setIcon(Bottonsakusei(x[5])));
         canvas.add(this.s6Button);
 
         this.s7Button =new JButton(icon);
         this.s7Button.setBounds(170, 250, 68, 100);
-        this.s7Button.addActionListener((e) -> s7Button.setIcon(icon7));
+        this.s7Button.addActionListener((e) -> s7Button.setIcon(Bottonsakusei(x[6])));
         canvas.add(this.s7Button);
 
         this.s8Button =new JButton(icon);
         this.s8Button.setBounds(240, 250, 68, 100);
-        this.s8Button.addActionListener((e) -> s8Button.setIcon(icon8));
+        this.s8Button.addActionListener((e) -> s8Button.setIcon(Bottonsakusei(x[7])));
         canvas.add(this.s8Button);
 
         this.s9Button =new JButton(icon);
         this.s9Button.setBounds(310, 250, 68, 100);
-        this.s9Button.addActionListener((e) -> s9Button.setIcon(icon9));
+        this.s9Button.addActionListener((e) -> s9Button.setIcon(Bottonsakusei(x[8])));
         canvas.add(this.s9Button);
 
         this.s10Button =new JButton(icon);
         this.s10Button.setBounds(380, 250, 68, 100);
-        this.s10Button.addActionListener((e) -> s10Button.setIcon(icon10));
+        this.s10Button.addActionListener((e) -> s10Button.setIcon(Bottonsakusei(x[9])));
         canvas.add(this.s10Button);
 
         // レイアウトマネージャの指定
@@ -137,5 +136,28 @@ public class MainWindow {
     public void show() {
         // フレームの表示
         this.frame.setVisible(true);
+    }
+    //ランダムにカードを作成
+    public ImageIcon Bottonsakusei(int s){
+        if(s==1)
+        return icon1;
+        else if(s==2)
+        return icon2;
+        else if(s==3)
+        return icon3;
+        else if(s==4)
+        return icon4;
+        else if(s==5)
+        return icon5;
+        else if(s==6)
+        return icon6;
+        else if(s==7)
+        return icon7;
+        else if(s==8)
+        return icon8;
+        else if(s==9)
+        return icon9;
+        else
+        return icon10;
     }
 }
